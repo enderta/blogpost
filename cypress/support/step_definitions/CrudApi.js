0e4330a547dfdb54033bb238f701ccd9295cb52f
enderta <endertanver@gmail.com>
2023-12-14 17:55:41 +0000

    Update user edit action in CrudUI tests
    The user edit action in CrudUI tests now corresponds to changing the author of a post, instead of its content. The tests related to post deletion were also adjusted. The check for a visible post on the Blog Homepage now validates author presence rather than content. Comments referring to unused code have been removed and new code segments were added to handle the functional shift.

--- cypress/support/step_definitions/CrudApi.js
+++ cypress/support/step_definitions/CrudApi.js
@@ -3,8 +3,7 @@ import { Given, When } from '@badeball/cypress-cucumber-preprocessor'
 let token;
 let id;
 
-
-const performRequest = ( query, variables) => {
+const performRequest = (query, variables) => {
     return cy.request({
         method: 'POST',
         url: Cypress.env('REACT_APP_GRAPHQL_ENDPOINT'),
@@ -20,7 +19,7 @@ const performRequest = ( query, variables) => {
     });
 }
 
-Given("I am an authenticated user", () => {
+const loginUser = () => {
     const query = `
         mutation loginUser($username: String!, $password: String!) {
             loginUser(username: $username, password: $password) {
@@ -28,16 +27,13 @@ Given("I am an authenticated user", () => {
             }
         }`;
 
-    performRequest( query, {
+    return performRequest(query, {
         username: Cypress.env('username'),
         password: Cypress.env('password'),
-    }).then((res) => {
-        token = res.body.data.loginUser.token;
-        expect(res.status).to.equal(200);
     });
-});
+}
 
-When("I perform a POST request to {string} with valid payload", (url) => {
+const createBlogPost = () => {
     const query = `
         mutation Mutation($title: String!, $content: String!, $author: String!, $imageUrl: String!) {
             createBlogPost(title: $title, content: $content, author: $author, image_url: $imageUrl) {
@@ -45,18 +41,15 @@ When("I perform a POST request to {string} with valid payload", (url) => {
             }
         }`;
 
-    performRequest( query, {
+    return performRequest(query, {
         title: 'Test Title',
         content: 'Test Content',
         author: 'Test Author',
         imageUrl: 'Test Image URL',
-    }).then((res) => {
-        id = res.body.data.createBlogPost.id;
-        expect(res.status).to.equal(200);
     });
-});
+}
 
-When("I perform a POST request to {string} with valid payload for update", (url) => {
+const updateBlogPost = () => {
     const query = `
         mutation Mutation($updateBlogPostId: ID!, $title: String!, $content: String!, $author: String!) {
             updateBlogPost(id: $updateBlogPostId, title: $title, content: $content, author: $author) {
@@ -64,17 +57,15 @@ When("I perform a POST request to {string} with valid payload for update", (url)
             }
         }`;
 
-    performRequest( query, {
+    return performRequest(query, {
         updateBlogPostId: id,
         title: 'Test Title',
         content: 'Test Content',
         author: 'Test Author',
-    }).then((res) => {
-        expect(res.status).to.equal(200);
     });
-});
+}
 
-When("I perform a DELETE request to {string}", (url) => {
+const deleteBlogPost = () => {
     const query = `
         mutation DeleteBlogPost($deleteBlogPostId: ID!) {
             deleteBlogPost(id: $deleteBlogPostId) {
@@ -82,9 +73,33 @@ When("I perform a DELETE request to {string}", (url) => {
             }
         }`;
 
-    performRequest(query, {
+    return performRequest(query, {
         deleteBlogPostId: id,
-    }).then((res) => {
+    });
+}
+
+Given("I am an authenticated user", () => {
+    loginUser().then((res) => {
+        token = res.body.data.loginUser.token;
+        expect(res.status).to.equal(200);
+    });
+});
+
+When("I perform a POST request to {string} with valid payload", (url) => {
+    createBlogPost().then((res) => {
+        id = res.body.data.createBlogPost.id;
+        expect(res.status).to.equal(200);
+    });
+});
+
+When("I perform a POST request to {string} with valid payload for update", (url) => {
+    updateBlogPost().then((res) => {
+        expect(res.status).to.equal(200);
+    });
+});
+
+When("I perform a DELETE request to {string}", (url) => {
+    deleteBlogPost().then((res) => {
         expect(res.status).to.equal(200);
     });
 });
