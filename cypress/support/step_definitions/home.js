73356618d6193e733cd3eea4f3967780db568c7c
enderta <endertanver@gmail.com>
2023-12-20 13:44:17 +0000

    database test added

--- cypress/support/step_definitions/home.js
+++ cypress/support/step_definitions/home.js
@@ -1,9 +1,11 @@
 import {Given, When, Then} from '@badeball/cypress-cucumber-preprocessor'
+
 require("cypress-xpath");
 
 let numberofpostsUI;
 
 Given('I am on the main page I see the {string} text', (expectedWelcomeText) => {
+
     cy.wait(5000);
     cy.visit('https://blogpost-flax.vercel.app/');
 
